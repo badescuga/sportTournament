b99d6a30023af2d1ebe8e691273d7959e6238a4b
lucipop <lucipop1@hotmail.com>
2016-03-28 01:53:28 +0300

    Modified the devtool attribute to work correctly

--- mobileApp/webpack.config.js
+++ mobileApp/webpack.config.js
@@ -4,7 +4,6 @@ var path = require('path');
 module.exports = {
     devtool: '#eval-source-map',
   entry: [
-    
     path.normalize('es6-shim/es6-shim.min'),
     'reflect-metadata',
     path.normalize('zone.js/dist/zone-microtask'),
